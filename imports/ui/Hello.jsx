556f2a36563436deb8a79e2b41043373584a148f
Pedro <pedrogh.godoy12@gmail.com>
2023-07-05 23:13:57 -0300

    first commit

---
+++ imports/ui/Hello.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export const Hello = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={increment}>Click Me</button>
+      <p>Hello {counter} times.</p>
+    </div>
+  );
+};
